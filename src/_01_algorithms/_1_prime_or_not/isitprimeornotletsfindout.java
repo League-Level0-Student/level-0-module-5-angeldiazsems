ba683bf2437693322bbb87fdcb05a4dfbf3085a8
root <root@886008c73cea>
2020-06-10 21:55:29 +0000

    asdfghjkl;'

---
+++ src/_01_algorithms/_1_prime_or_not/isitprimeornotletsfindout.java
@@ -0,0 +1,54 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class isitprimeornotletsfindout {
+public static void main(String[] args) {
+	
+	
+String input =	JOptionPane.showInputDialog("Give me a number");
+	
+	
+
+
+
+	for (int i = 0; i < input; i++) {
+	
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
