3193dad5637fc0e706c3cf591a216edf39febb79
root <root@8d21d1874d85>
2020-07-22 21:55:52 +0000

    e

--- src/_02_nested_loops/_3_for_loop_gauntlet/loops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/loops.java
@@ -1,23 +1,88 @@
 package _02_nested_loops._3_for_loop_gauntlet;
 
+import javax.swing.JOptionPane;
+
 public class loops {
 public static void main(String[] args) {
+	
 	
 	/*
 	for (int i = 0; i < 101; i++) {
 		
 	System.out.println(i);
 	}
-	
-*/
+
 	
 	
 	
 
-	for (int i = 100; i < ; i--) {
+	for (int i = 101; i > 0; i--) {
 		
 	System.out.println(i);
 	}
+	
+	
+	
+	
+	
+	for (int i = 2; i < 101; i++) {
+	if(i%2==0) {
+		System.out.println(i);
+	}
+	}
+	
+	
+	
+	
+	
+
+	for (int i = 1; i < 100; i++) {
+	if(i%2==1) {
+		System.out.println(i);
+	}
+	}
+	
+	
+	
+	for (int i = 0; i < 501; i++) {
+	if(i%2==0) {
+		System.out.println(i+" is an even number");
+	}
+	else if(i%2==1) {
+		System.out.println(i+" is an odd number");
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	for (int i = 0; i < 778; i++) {
+		
+	
+		if(i%7==0) {
+		
+		System.out.println(i);
+		}
+	}
+	
+	
+	*/                     
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
