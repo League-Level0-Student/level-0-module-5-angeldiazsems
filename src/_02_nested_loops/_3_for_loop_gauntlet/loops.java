afbb3b30b9b9d9632e7c8ffe68dee1a28906e614
root <root@01f50927ed90>
2020-08-05 21:55:36 +0000

    i am being watched

--- src/_02_nested_loops/_3_for_loop_gauntlet/loops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/loops.java
@@ -80,18 +80,17 @@ public static void main(String[] args) {
 	
 	for (int i = 0; i < 3; i++) {
 		
-	
-		System.out.println();
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
 		
-		for (int j = 0; j < args.length; j++) {
-		System.out.println("0");
-		System.out.println("1");	
-		System.out.println("2");
+	
+	for (int j = 0; j < 3; j++) {
+		//System.out.println("1");
+		//System.out.println("2");
+		//System.out.println("3");
 	}
-	
-	
-	
-	
+	}
 	}
 	
 	
@@ -119,4 +118,4 @@ public static void main(String[] args) {
 	
 	
 }
-}
+
