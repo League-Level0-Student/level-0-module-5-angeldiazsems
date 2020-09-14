5b15078ee82b39535a01ac5527a676a4fef89a19
root <root@9a17f8c21916>
2020-09-14 20:27:42 +0000

    cody minus sauer is -1

--- src/_04_practice/_1_skill_practice/SkillPractice.java
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -1,5 +1,7 @@
 package _04_practice._1_skill_practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
@@ -8,8 +10,8 @@ public static void main(String[] args) {
 	
 	SkillPractice skills = new SkillPractice();
     skills.skill1();
-    //skills.skill2();
-  //  skills.skill3();
+    skills.skill2();
+   skills.skill3();
    // skills.skill4();
    // skills.skill5();
 }
@@ -18,17 +20,38 @@ public static void main(String[] args) {
 void skill1() {
 	String input = JOptionPane.showInputDialog("how many dimes do you have");
 	int hi =Integer.parseInt(input);
-	
 	int coins = hi*10;
-
 JOptionPane.showMessageDialog(null, "you have "+coins+" coins in total");
-
+String input2 = JOptionPane.showInputDialog("How tall are you in inches?");
+int hi2 = Integer.parseInt(input2);
+if(hi2 < 36) {
+	JOptionPane.showMessageDialog(null, "You need to eat your Wheaties(boomer joke)");
+}
 
 }
 
+void skill2() {
+	
+	for(int i = 0; i < 30; i++ ) {
+		if(i%3 == 2) {
+		System.out.println(i);
+		}
+	}
+}
 
+void skill3() {
+	
+	Random ran = new Random();
+	int cody = ran.nextInt(20);
+	System.out.println(cody);
+	Random road = new Random();
+	int sauer = ran.nextInt(10);
+	System.out.println(sauer);
 
-
+JOptionPane.showMessageDialog(null, "the difference between the two numbers is "+(cody-sauer));
+	
+	
+}
 
 
 
